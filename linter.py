5fe15c02359a7456e921e5f991bac299f85a831e
Koen Lageveen <koen.lageveen@me.com>
2018-02-05 12:24:36 +0100

    WARNING has been moved in SL4

--- linter.py
+++ linter.py
@@ -10,7 +10,15 @@
 
 """This module exports the Foodcritic plugin class."""
 
-from SublimeLinter.lint import RubyLinter, highlight, util
+from SublimeLinter.lint import RubyLinter, util
+
+import SublimeLinter
+if getattr(SublimeLinter.lint, 'VERSION', 3) > 3:
+    from SublimeLinter.lint import const
+    WARNING = const.WARNING
+else:
+    from SublimeLinter.lint import highlight
+    WARNING = highlight.WARNING
 
 
 class Foodcritic(RubyLinter):
@@ -23,7 +31,7 @@ class Foodcritic(RubyLinter):
     version_re = r'(?P<version>\d+\.\d+\.\d+)'
     version_requirement = '>= 2.2.0'
     regex = r'(?P<message>FC\d+: .+): (?P<file>.+):(?P<line>.+)'
-    default_type = highlight.WARNING
+    default_type = WARNING
     multiline = False
     line_col_base = (1, 1)
     tempfile_suffix = 'rb'
